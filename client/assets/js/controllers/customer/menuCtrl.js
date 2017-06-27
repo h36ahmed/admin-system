95746aca8195b591bc1cc7d250a3384ea97d63a0
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-06-27 16:39:19 -0400

    Customers are now redirected back to order page if they already have an order

--- client/assets/js/controllers/customer/menuCtrl.js
+++ client/assets/js/controllers/customer/menuCtrl.js
@@ -2,19 +2,27 @@ var app = angular.module('lunchSociety');
 
 var menuCtrl = function ($scope, $location, $window, utilService, modalService, commonService, orderService) {
 
-    // https://stackoverflow.com/questions/24940320/how-to-redirect-using-ng-click
-    // this is where i got the function. not sure if this is the right way to do it
+    const customer = commonService.getCustomerID()
     if ($window.sessionStorage.feedbackID) {
       $location.path(`create-feedback/${$window.sessionStorage.feedbackID}`)
     }
 
     $scope.redirect = function(url, refresh) {
-      if (refresh || $scope.$$phase) {
-        utilService.isKitchenOpen() ? $window.location.href = url : $window.location.href = '/#!/kitchen-closed'
-      } else {
-        utilService.isKitchenOpen() ? $location.path(url) : $location.path('/#!/kitchen-closed')
-        $scope.$apply();
-      }
+      utilService.checkCustomerOrdered({customer_id: customer, order_date: moment().format('YYYY-MM-DD'), status: 'active'})
+        .then(data => {
+          if (data.data.length > 0) {
+            $location.path(`order/${data.data[0].id}`)
+
+          // https://stackoverflow.com/questions/24940320/how-to-redirect-using-ng-click
+          // this is where i got the function. not sure if this is the right way to do it
+          } else if (refresh || $scope.$$phase) {
+            utilService.isKitchenOpen() ? $window.location.href = url : $window.location.href = '/#!/kitchen-closed'
+          } else {
+            utilService.isKitchenOpen() ? $location.path(url) : $location.path('/#!/kitchen-closed')
+            $scope.$apply();
+          }
+        })
+
     }
 
     $scope.logout = function() {
